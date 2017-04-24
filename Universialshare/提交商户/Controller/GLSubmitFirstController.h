2e20314b9737faf6c85b4e7f6d6a396d642c4f1a
龚磊 <1029235277@qq.com>
2017-04-24 09:29:04 +0800

    no message

---
+++ Universialshare/提交商户/Controller/GLSubmitFirstController.h
@@ -0,0 +1,13 @@
+//
+//  GLSubmitFirstController.h
+//  Universialshare
+//
+//  Created by 龚磊 on 2017/4/24.
+//  Copyright © 2017年 四川三君科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GLSubmitFirstController : UIViewController
+
+@end
---
+++ Universialshare/提交商户/Controller/GLSubmitFirstController.m
@@ -0,0 +1,24 @@
+//
+//  GLSubmitFirstController.m
+//  Universialshare
+//
+//  Created by 龚磊 on 2017/4/24.
+//  Copyright © 2017年 四川三君科技有限公司. All rights reserved.
+//
+
+#import "GLSubmitFirstController.h"
+
+@interface GLSubmitFirstController ()
+
+@end
+
+@implementation GLSubmitFirstController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+
+
+@end
